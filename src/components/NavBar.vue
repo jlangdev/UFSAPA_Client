0ca8e20fc846048823438ee45f4d21d176a0ad99
Giovanni Gaspard <giovannigaspard@gmail.com>
2019-04-23 19:02:45 -0400

    Plz Dont Judge me and my Hamburger:

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-toolbar app color = '#CAEBF2'>
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title class = "title">UFSAPA</v-toolbar-title>
+  <v-toolbar app color="#CAEBF2">
+    <!-- <v-toolbar-side-icon></v-toolbar-side-icon>-->
+    <v-toolbar-title class="title">UFSAPA</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn v-if="isLoggedIn" flat>
@@ -19,7 +19,7 @@
       <v-btn v-if="isLoggedIn" flat>
         <router-link to="/profile" tag="li">My Profile</router-link>
       </v-btn>
-      <v-btn v-if="isLoggedIn" @click='logOut()' flat>
+      <v-btn v-if="isLoggedIn" @click="logOut()" flat>
         <router-link to="/login" tag="li">Log Out</router-link>
       </v-btn>
 
@@ -29,8 +29,68 @@
       <v-btn v-if="!isLoggedIn" flat>
         <router-link to="/register" tag="li">Register</router-link>
       </v-btn>
-
     </v-toolbar-items>
+    <v-menu class="hidden-md-and-up">
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-list>
+        <v-list-tile v-if="isLoggedIn">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/events" tag="li">Events</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/members" tag="li">Members</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/board" tag="li">Board Members</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/about" tag="li">About</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="isLoggedIn">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/profile" tag="li">My Profile</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="isLoggedIn">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/login" tag="li" v-if="isLoggedIn" @click="logOut()">Log Out</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="!isLoggedIn">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/login" tag="li">Login</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="!isLoggedIn">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link to="/register" tag="li">Register</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 
@@ -40,38 +100,36 @@ export default {
   props: {
     msg: String
   },
-  data(){
-    return{
-      isLoggedIn: false,
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  },
+  methods: {
+    logOut: function() {
+      delete localStorage.user;
+      delete localStorage.profile;
+      delete localStorage.token;
+      location.reload();
     }
   },
-  methods:{
-    logOut: function(){
-      delete localStorage.user
-      delete localStorage.profile
-      delete localStorage.token
-      location.reload()
-
-    }
-  },
-  mounted(){
-    if(localStorage.token){
-      this.isLoggedIn = true
-    }else{
-      this.isLoggedIn = false
+  mounted() {
+    if (localStorage.token) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
     }
   }
 };
 </script>
 
 <style>
-.hidden-sm-and-down{
+.hidden-sm-and-down {
   font-family: Nunito;
   /*font-weight: bold;*/
-
 }
 
-.title{
+.title {
   font-family: Nunito;
 }
 </style>
